f02a24a96b4989faf93e05197a3053d81ad015e0
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-17 09:12:25 +0600

    added some corrections of Navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -26,11 +26,14 @@ const Navbar = () => {
         },
     ];
 
-    // রেন্ডার করার সুবিধার্থে মেনু লিস্টটি আলাদা ভেরিয়েবলে রাখা হয়েছে
+
     const menuLinks = navItems.map((item, i) => (
         <MyLink key={i} href={item.path}>
             <div className='flex items-center gap-1'>
-                <Image src={item.img} alt={item.text} width={20} height={20} />
+                <Image src={item.img}
+                    alt={item.text}
+                    width={20}
+                    height={20} />
                 <span>{item.text}</span>
             </div>
         </MyLink>
@@ -42,13 +45,13 @@ const Navbar = () => {
 
                 <div className="navbar-start">
                     {/* For Small Device */}
-                    <div className="dropdown lg:hidden">
+                    <div className="dropdown md:hidden">
                         <div tabIndex={0} role="button" className="btn btn-ghost p-0 mr-2">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </div>
-                        <ul tabIndex={0} className="flex flex-col hover dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 border border-base-200">
+                        <ul tabIndex={0} className="flex flex-col dropdown-content mt-3 z-1 p-0 shadow bg-base-100 rounded-box w-52 border border-base-200">
                             {menuLinks}
                         </ul>
                     </div>
@@ -65,7 +68,7 @@ const Navbar = () => {
                 </div>
 
                 {/* For Large Device */}
-                <div className="navbar-end hidden lg:flex">
+                <div className="navbar-end hidden md:flex">
                     <ul className="flex justify-center items-center text-base font-medium text-[#64748B]">
                         {menuLinks}
                     </ul>
